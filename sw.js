ff708a346d9b7bd8c572098c14fecea36a8f153f
Cynthia Wright <cynsdaemon@users.noreply.github.com>
2019-01-16 20:28:47 -0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -16,15 +16,15 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open(staticCacheName).then(function(cache) {
         return cache.addAll([
-          '/', // this caches index.html
-          '/restaurant.html',
-          '/css/styles.css',
-          '/css/responsive.css',
-          '/js/dbhelper.js',
-          '/js/main.js',
-          '/js/restaurant_info.js',
-          'js/register-sw.js', 
-          'data/restaurants.json'
+          'https://cynsdaemon.github.io/restaurant-reviews-app/', // this caches index.html
+          'restaurant-reviews-app/restaurant.html',
+          'restaurant-reviews-app/css/styles.css',
+          'restaurant-reviews-app/css/responsive.css',
+          'restaurant-reviews-app/js/dbhelper.js',
+          'restaurant-reviews-app/js/main.js',
+          'restaurant-reviews-app/js/restaurant_info.js',
+          'restaurant-reviews-appjs/register-sw.js', 
+          'restaurant-reviews-app/data/restaurants.json'
           // add other static assets here
         ]);
       })
